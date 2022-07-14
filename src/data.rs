c0176742f039d63cb892f0fd955f1b8e63ac7154
Alisander Qoshqosh <qalisander@gmail.com>
2022-07-14 12:12:58 +0400

    data models file

---
+++ src/data.rs
@@ -0,0 +1,61 @@
+use serde::{Deserialize, Serialize};
+
+//usage of #[serde(tag = "type")] is not possible because of bug
+//https://github.com/BurntSushi/rust-csv/issues/278
+#[derive(Deserialize)]
+pub(crate) struct TrCsvRow {
+    #[serde(alias = "type")]
+    tr_type: String,
+    client: u16,
+    tx: u32,
+    amount: Option<f64>,
+}
+
+//client, available, held, total, locked
+#[derive(Serialize)]
+pub(crate) struct ClientInfo{
+    client: u16,
+    available: f64,
+    held: f64,
+    total: f64,
+    locked: bool,
+}
+
+pub(crate) struct Account {
+    available: f64,
+    held: f64,
+    locked: bool,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub(crate) enum Tr {
+    Deposit { client: u16, tx: u32,  amount: f64 },
+    Withdrawal { client: u16, tx: u32, amount: f64 },
+    Dispute { client: u16, tx: u32 },
+    Resolve { client: u16, tx: u32 },
+    Chargeback { client: u16, tx: u32 },
+}
+
+impl TryFrom<TrCsvRow> for Tr {
+    type Error = &'static str;
+
+    fn try_from(csv_row: TrCsvRow) -> std::result::Result<Self, Self::Error> {
+        let client = csv_row.client;
+        let tx = csv_row.tx;
+
+        match &*csv_row.tr_type {
+            "deposit" => match csv_row.amount {
+                None => Err("Not valid amount!"),
+                Some(amount) => Ok(Tr::Deposit { client, tx, amount }),
+            },
+            "withdrawal" => match csv_row.amount {
+                None => Err("Not valid amount!"),
+                Some(amount) => Ok(Tr::Withdrawal { client, tx, amount }),
+            },
+            "dispute" => Ok(Tr::Dispute {tx, client}),
+            "resolve" => Ok(Tr::Resolve {tx, client}),
+            "chargeback" => Ok(Tr::Chargeback {tx, client}),
+            _ => Err("Not valid csv row!"),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,14 @@
 mod tr_processor;
+mod data;
 
 use anyhow::Result;
 use csv::Trim;
-use serde::{Deserialize, Serialize};
 use std::fmt::Error;
 use std::io;
 
-//usage of #[serde(tag = "type")] is not possible because of bug
-//https://github.com/BurntSushi/rust-csv/issues/278
-#[derive(Deserialize)]
-struct TrCsvRow {
-    #[serde(alias = "type")]
-    tr_type: String,
-    client: u16,
-    tx: u32,
-    amount: Option<f64>,
-}
 
 
-#[derive(Debug, Copy, Clone)]
-enum Tr {
-    Deposit { client: u16, tx: u32,  amount: f64 },
-    Withdrawal { client: u16, tx: u32, amount: f64 },
-    Dispute { client: u16, tx: u32 },
-    Resolve { client: u16, tx: u32 },
-    Chargeback { client: u16, tx: u32 },
-}
 
-impl TryFrom<TrCsvRow> for Tr {
-    type Error = &'static str;
-
-    fn try_from(csv_row: TrCsvRow) -> std::result::Result<Self, Self::Error> {
-        let client = csv_row.client;
-        let tx = csv_row.tx;
-
-        match &*csv_row.tr_type {
-            "deposit" => match csv_row.amount {
-                None => Err("Not valid amount!"),
-                Some(amount) => Ok(Tr::Deposit { client, tx, amount }),
-            },
-            "withdrawal" => match csv_row.amount {
-                None => Err("Not valid amount!"),
-                Some(amount) => Ok(Tr::Withdrawal { client, tx, amount }),
-            },
-            "dispute" => Ok(Tr::Dispute {tx, client}),
-            "resolve" => Ok(Tr::Resolve {tx, client}),
-            "chargeback" => Ok(Tr::Chargeback {tx, client}),
-            _ => Err("Not valid csv row!"),
-        }
-    }
-}
 // TODO: abstract over iterator of transaction
 // TODO: pay attention to f64, use decimal
 // https://docs.rs/rust_decimal/latest/rust_decimal/
