3cc2f55530c2f4fe1d2288df98eef62f30e28c07
Alisander Qoshqosh <qalisander@gmail.com>
2022-07-14 22:28:05 +0400

    main file reader

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,50 @@
-mod tr_processor;
-mod data;
-use anyhow::Result;
-use csv::Trim;
+use std::env::args;
+use std::fs::File;
 use std::io;
+
+use anyhow::{Error, Result};
+use csv::Trim;
+
 use crate::data::*;
 use crate::tr_processor::TrProcessor;
 
+mod data;
+mod tr_processor;
 
 // TODO: pay attention to f64, use decimal https://docs.rs/rust_decimal/latest/rust_decimal/
 // TODO: use another writer io::stderr() for errors
 
-fn main() -> Result<()> {
+fn main() {
+    let string = args().nth(1).expect("No file argument!");
+    let file = File::open(string).unwrap();
     let mut reader = csv::ReaderBuilder::new()
         .flexible(true)
         .trim(Trim::All)
         .comment(Some(b'#'))
-        .from_reader(io::stdin());
-    for result in reader.deserialize() {
-        let tr_csv_row: TrRecord = result?;
-        let tr: Tr = tr_csv_row.try_into().unwrap();
-        println!("{:?}", tr);
-    }
+        .from_reader(file);
 
-    let processor = TrProcessor::new();
+    let iter = reader
+        .deserialize()
+        .into_iter()
+        .map::<Result<Tr>, _>(|record| {
+            let record: TrRecord = record?;
+            let tr: Tr = record.try_into()?;
+            Ok(tr)
+        })
+        .inspect(|res| {
+            if let Err(err) = res {
+                eprintln!("{}", err)
+            }
+        })
+        .flatten();
     
+    let mut processor = TrProcessor::new();
+    processor.process(iter);
     let mut writer = csv::Writer::from_writer(io::stdout());
     for info in processor.get_client_records() {
-        writer.serialize(info)?;
+        writer.serialize(info).unwrap_or_else(|err|{
+            eprintln!("{}", err)
+        });
     }
-
-    Ok(())
 }
+
